96de0549f81a7e99dafe31df5c7b0d696aadf972
Pandemist <danielc.b@web.de>
2017-07-16 17:35:23 +0200

    fixed some things

--- code/fixed.java
+++ code/fixed.java
@@ -9,25 +9,6 @@ public class Program {
 				if (lang.equals("JAVA"))
 					return com.astortest.Language.JAVA;
 				else
-					return com.astortest.Language.PYTHON;
-				
-			
-		
-	}
-
-	public static com.astortest.Language working(java.lang.String lang) {
-		if (lang.equals("C"))
-			return com.astortest.Language.C;
-		else
-			if (lang.equals("CPP"))
-				return com.astortest.Language.CPP;
-			else
-				if (lang.equals("JAVA"))
-					return com.astortest.Language.JAVA;
-				else
-					return com.astortest.Language.PYTHON;
-				
-			
-		
+					return com.astortest.Language.PYTHON;		
 	}
 }
---
+++ code/fixed_.java
@@ -0,0 +1,33 @@
+public class Program {
+	public com.astortest.Language getLanguage(java.lang.String lang) {
+		if (lang.equals("C"))
+			return com.astortest.Language.C;
+		else
+			if (lang.equals("CPP"))
+				return com.astortest.Language.CPP;
+			else
+				if (lang.equals("JAVA"))
+					return com.astortest.Language.JAVA;
+				else
+					return com.astortest.Language.PYTHON;
+				
+			
+		
+	}
+
+	public static com.astortest.Language working(java.lang.String lang) {
+		if (lang.equals("C"))
+			return com.astortest.Language.C;
+		else
+			if (lang.equals("CPP"))
+				return com.astortest.Language.CPP;
+			else
+				if (lang.equals("JAVA"))
+					return com.astortest.Language.JAVA;
+				else
+					return com.astortest.Language.PYTHON;
+				
+			
+		
+	}
+}
